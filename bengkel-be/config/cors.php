628e289db64301bd46e4e12704bf5f76bf0f10e5
sukaastw <msuka5641@gmail.com>
2025-12-19 15:54:39 +0800

    tambah cors1

--- bengkel-be/config/cors.php
+++ bengkel-be/config/cors.php
@@ -12,7 +12,7 @@ return [
     'http://localhost:3001',
     'http://127.0.0.1:3000',
     'http://127.0.0.1:3001',
-    'https://tekweb-pjhujd3cd-blangkos-projects.vercel.app',
+    'https://tekweb-ra1tp4ezw-blangkos-projects.vercel.app/',
 ],
 
     'allowed_origins_patterns' => [],
