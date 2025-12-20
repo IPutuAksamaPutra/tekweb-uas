9f8d1037ff0d0eff67f7f258e2600ae6153daafd
Komang Verdy <komangverdy8@gmail.com>
2025-12-20 19:44:45 +0800

    rubah metadata layout

--- bengkel-app/app/layout.tsx
+++ bengkel-app/app/layout.tsx
@@ -4,15 +4,19 @@ import "./globals.css";
 export const metadata = {
   title: "BengkelApp",
   description: "Website Bengkel",
+  verification: {
+    google: "LB5mbgIAoSnXWJFkXZgX_wsg76j7ZqG1fQC3DL4Wmvk",
+  },
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
-      {/* pastikan body class konsisten di sini */}
-      <body className="min-h-screen bg-gray-100">
-        {children}
-      </body>
+      <body className="min-h-screen bg-gray-100">{children}</body>
     </html>
   );
 }
