876c3ae94a596691b5960270c9271fe523b9b8a2
Komang Verdy <komangverdy8@gmail.com>
2025-12-21 14:52:36 +0800

    edit2

--- bengkel-app/components/user/FooterUsr.tsx
+++ bengkel-app/components/user/FooterUsr.tsx
@@ -1,8 +1,12 @@
 export default function FooterUsr() {
   return (
-    <footer className="w-full text-gray-300 py-4 mt-10" style={{ backgroundColor: "#234C6A" }}>
+    <footer
+      className="w-full text-gray-300 py-4 mt-10"
+      style={{ backgroundColor: "#234C6A" }}
+    >
       <div className="container mx-auto text-center text-sm">
-        © {new Date().getFullYear()} Bengkel App — Create By : Team Yapping
+        © {new Date().getFullYear()} Bengkel App — Create with ♥ by team Bengkel
+        App
       </div>
     </footer>
   );
